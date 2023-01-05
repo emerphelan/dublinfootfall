6ac7fe13a1a312dafd027dd9333246c55fb27985
emerphelan <emerhphelan@gmail.com>
2023-01-05 17:43:57 +0000

    Changes to ROxygen documentation in fit_footfall. Changes to plot_footfall.

--- R/fit_footfall.R
+++ R/fit_footfall.R
@@ -4,7 +4,7 @@
 #' monthly data for the Dublin 2021 footfall data set. It returns an object
 #' containing a variety of values of potential interest.
 #'
-#' @param x The Dublin 2021 footfall data, from the \code{"load_footfall()"}
+#' @param dat The Dublin 2021 footfall data, from the \code{"load_footfall()"}
 #' function.
 #' @param data_type Whether the model should examine daily, weekly
 #' or monthly data. Set to daily by default.
@@ -23,11 +23,14 @@
 #' @author Emer Phelan - </{emer.phelan.2019@@mumail.ie}>
 #' @seealso \code{\link{fit}}, \code{\link{load_footfall}}
 #'
+#' @importFrom lubridate "wday" "week" "month"
+#' @importFrom stats "lm"
+#'
 #' @examples
 #' dublin <- load_footfall()
-#' fit1 <- fit.footfall(dublin)
-#' fit2 <- fit.footfall(dublin, mod_type = "weekly")
-#' fit3 <- fit.footfall(dublin, mod_type = "monthly")
+#' fit1 <- fit_footfall(dublin)
+#' fit2 <- fit_footfall(dublin, mod_type = "weekly")
+#' fit3 <- fit_footfall(dublin, mod_type = "monthly")
 #'
 #' plot(fit1$mod.fitted, fit1$mod.residuals)
 #' fit2$mod.sigma
--- R/plot_footfall.R
+++ R/plot_footfall.R
@@ -1,37 +1,69 @@
-# plot  for Dublin 2021 footfall data.
+#' Visualisation for Dublin 2021 footfall data.
 #'
-#' The function creates a plot for the Dublin 2021 footfall data
-#' It returns a plot for the count with respect to location
-#' It also returns a plot for the residuals with respect to fitted values
-#' fit_footfall default function.
-#' @example
+#' The function creates multiple plots relating to the Dublin 2021 footfall
+#' data set. It creates one of three plots: a scatter plot with mean footfall
+#' counts, a scatter plot with total footfall counts and a residual vs fitted
+#' plot for a linear regression created from the data.
+#'
+#' @param dat The Dublin 2021 footfall data, from the \code{"load_footfall()"}
+#' function.
+#' @param mod An object of type \code{"lrmodel"}, from the
+#' \code{"fit_footfall()"} function. If no object is provided, one is created
+#' by default.
+#' @param plot_type The type of plot produced. Can be "mean" for the mean
+#' footfall plot, "total" for the total footfall plot or "res vs fit" for the
+#' residual vs fitted plot. Set to residual vs fitted plot by default.
+#'
+#' @return One of three plots:
+#' A scatter plot of mean footfall counts across each Dublin location,
+#' A scatter plot of total footfall counts across each Dublin location,
+#' A residual vs fitted plot from the linear model object.
+#'
+#' @export
+#'
+#' @author Obinna Njoku - </{Obinna.Njoku.2019@@mumail.ie}>
+#' @seealso \code{\link{load_footfall}}, \code{\link{fit_footfall}}
+#'
+#' @importFrom tidyr "group_by" "mutate"
+#' @importFrom ggplot2 "ggplot" "geom_point" "theme_bw" "labs"
+#'
+#' @examples
 #' dublin <- load_footfall()
-#' fit1 <- fit.footfall(dublin)
-#' plot_footfall(dublin,fit1)
+#' fit1 <- fit_footfall(dublin)
 #'
-#' @param NA
-#' @author Obinna Njoku - <\email{Obinna.Njoku.2019@@mumail.ie}
+#' plot_footfall(dublin, fit1)
+#' plot(dublin, fit1, plot_type = "mean")
 
+plot_footfall<- function(dat,
+                         mod = fit_footfall(dat),
+                         plot_type = "res vs fit"){
 
-library(tidyverse)
-plot_footfall<- function(dat,fit){
- x<- as.data.frame(dat)
+dat1 <- dat |>
+    tidyr::group_by(Location) |>
+    tidyr::mutate(mCount = mean(Count),
+                  tCount = sum(Count))
 
-fit1 <- as.data.frame(fit)
+if(plot_type == "mean") {
 
-ggplot(x,aes(x=Location, y=Count))+
-  geom_point()+
-  geom_line(colour="red")+
-  theme_bw()+
-  labs(title = "Data visualisation")
+  ggplot2::ggplot(data = dat1, aes(x = mCount, y = Location))+
+      geom_point()+
+      theme_bw()+
+      labs(title = "Mean Footfall for Each Dublin Location in 2021",
+           x = "Mean Count")
 
-ggplot(x,aes(x=Location, y=Count))+
-  geom_boxplot()+
-  geom_line(colour="red")+
-  theme_bw()+
-  labs(title = "Data visualisation")
+} else if (data_type == "total") {
 
-plot(fit1$mod.fitted, fit1$mod.residuals, xlab = "Fitted" , ylab = "Residuals")
+  ggplot2::ggplot(data = dub_dat1, aes(x = tCount, y = Location))+
+    geom_point()+
+    theme_bw()+
+    labs(title = "Total Footfall for Each Dublin Location in 2021",
+         x = "Total Count")
+
+  } else {
+
+    # for monthly
+    plot(mod$mod.fitted, mod$mod.residuals)
+  }
 }
 
 
